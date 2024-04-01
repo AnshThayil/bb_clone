8a6f1c966b490ba4c25489fe00d4299f7fecfdbe
Ansh Thayil <ansh.thayil@yahoo.in>
2024-04-01 15:12:00 +0530

    View Boulder Page

--- bb_clone/urls.py
+++ bb_clone/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gyms.urls'))
+    path('', include('gyms.urls')),
+    path('boulder/', include('boulders.urls'))
 ]
--- boulders/views.py
+++ boulders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Boulder
 
 # Create your views here.
+def boulder(request, pk):
+    boulder = Boulder.objects.get(pk=pk)
+    context = {
+        'boulder': boulder,
+        'annotations': boulder.annotation_set.all()
+    }
+
+    return render(request, 'boulders/boulder.html', context)
