ff27d05633cf4d5c5dd62fdc0dd561c4778c8986
Ansh Thayil <ansh.thayil@yahoo.in>
2024-04-03 15:01:02 +0530

    Added buttons for gym management

--- gyms/models.py
+++ gyms/models.py
@@ -31,6 +31,7 @@ class Gym(models.Model):
     def __str__(self):
         return self.name
 
+
     def get_absolute_url(self):
         return reverse('gym_detail', kwargs={'pk': self.pk})
 
--- gyms/urls.py
+++ gyms/urls.py
@@ -4,6 +4,7 @@ from .views import GymListView, GymDetailView, GymCreateView
 
 urlpatterns = [
     path('', GymListView.as_view(), name='home'),
+    # path('gym/', views.home),
     path('gym/<int:pk>', GymDetailView.as_view(), name='gym_detail'),
     path('gym/create', GymCreateView.as_view(), name='gym_create')
 ]
