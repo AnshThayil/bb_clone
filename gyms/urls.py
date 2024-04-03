4ba4e6c820cf06c4093d22913e2b303ffe6bb44f
Ansh Thayil <ansh.thayil@yahoo.in>
2024-04-03 15:00:22 +0530

    Removed some whitespace

--- gyms/urls.py
+++ gyms/urls.py
@@ -4,7 +4,6 @@ from .views import GymListView, GymDetailView, GymCreateView
 
 urlpatterns = [
     path('', GymListView.as_view(), name='home'),
-    # path('gym/', views.home),
     path('gym/<int:pk>', GymDetailView.as_view(), name='gym_detail'),
     path('gym/create', GymCreateView.as_view(), name='gym_create')
 ]
--- gyms/views.py
+++ gyms/views.py
@@ -5,29 +5,6 @@ from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 
-
-def home(request):
-    context = {
-        'gyms': Gym.objects.all(),
-        'title': 'Gyms'
-    }
-
-    return render(request, 'gyms/home.html', context)
-
-def gym(request, pk):
-    gym = Gym.objects.get(pk=pk)
-    boulders = Boulder.objects.filter(wall__gym = gym)
-    
-    context = {
-        'gym': gym, 
-        'title':  gym.name,
-        'boulders': boulders,
-
-    }
-
-    return render(request, 'gyms/gym.html', context)
-
-
 class GymListView(ListView):
     model = Gym
     context_object_name = 'gyms'
@@ -40,7 +17,6 @@ class GymDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["boulders"] = Boulder.objects.all().filter(wall__gym = context['gym'])
         return context
-    
 
 class GymCreateView(CreateView):
     model = Gym
